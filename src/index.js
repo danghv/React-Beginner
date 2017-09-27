288c91f4b3c56ec9d64e90f7675a4919034f0db3
techmaster <techmaster@techmasters-Mac-mini-3.local>
2017-09-27 22:13:20 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {render} from 'react-dom'
+// day la kieu moi import App from './app'
+// classic
+// const App = require('./app')
+import App from './app'
+render(
+	<App />,
+	document.getElementById('root')
+)
